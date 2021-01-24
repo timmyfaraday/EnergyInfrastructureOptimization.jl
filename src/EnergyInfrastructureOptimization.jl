985710e1ea24e89632bbc79827f5932597c45f32
Tom Van Acker <tom.vanacker@esat.kuleuven.be>
2021-01-24 13:56:26 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/EnergyInfrastructureOptimization.jl
@@ -0,0 +1,5 @@
+module EnergyInfrastructureOptimization
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using EnergyInfrastructureOptimization
+using Test
+
+@testset "EnergyInfrastructureOptimization.jl" begin
+    # Write your tests here.
+end
